034ee8d3d64eeb920983f0676a88d6d0d6849488
Mamta Singh <mamta252002singh@gmail.com>
2025-09-12 14:59:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // 👈 make sure this is http
+        target: "https://slack-6u84.onrender.com", // 👈 make sure this is http
         changeOrigin: true,
         secure: false, // disable SSL check
       },
